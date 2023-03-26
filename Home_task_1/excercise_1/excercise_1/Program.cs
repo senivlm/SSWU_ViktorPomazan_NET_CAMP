43bc572f331d6900f4ee438a1ef3cd9a680e76a7
Kirieshka <67062457+Kirieshke@users.noreply.github.com>
2023-03-26 15:09:28 +0300

    init excercise_2

--- Home_task_1/excercise_1/excercise_1/Program.cs
+++ Home_task_1/excercise_1/excercise_1/Program.cs
@@ -1,2 +1,75 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int n = int.Parse(Console.ReadLine()); // кількість рядків
+int m = int.Parse(Console.ReadLine()); // кількість стовпців
+
+int[,] matrix = new int[n, m]; // ініціалізація матриці
+
+int value = 1; // початкове значення
+int row = 0; // поточний рядок
+int col = 0; // поточний стовпець
+int direction = 3;
+
+// заповнення матриці
+for (int i = 0; i < n * m; i++)
+{
+    matrix[row, col] = value; // присвоєння значення елементу матриці
+    value++; // збільшення значення для наступного елементу
+
+    // визначення наступного рядка і стовпця залежно від напряму руху
+    switch (direction)
+    {
+        case 0: // вниз
+            if (row == n - 1 || matrix[row + 1, col] != 0)
+            {
+                direction = 1; // зміна напряму руху на вправо
+                col++;
+            }
+            else
+            {
+                row++;
+            }
+            break;
+        case 1: // вправо
+            if (col == m - 1 || matrix[row, col + 1] != 0)
+            {
+                direction = 2; // зміна напряму руху на вверх
+                row--;
+            }
+            else
+            {
+                col++;
+            }
+            break;
+        case 2: // вверх
+            if (row == 0 || matrix[row - 1, col] != 0)
+            {
+                direction = 3; // зміна напряму руху на вліво
+                col--;
+            }
+            else
+            {
+                row--;
+            }
+            break;
+        case 3: // вліво
+            if (col == 0 || matrix[row, col - 1] != 0)
+            {
+                direction = 0; // зміна напряму руху на вниз
+                row++;
+            }
+            else
+            {
+                col--;
+            }
+            break;
+    }
+}
+
+// виведення матриці
+for (int i = 0; i < n; i++)
+{
+    for (int j = 0; j < m; j++)
+    {
+        Console.Write(matrix[i, j] + " ");
+    }
+    Console.WriteLine();
+}
---
+++ Home_task_1/excercise_2/excercise_2/Program.cs
@@ -0,0 +1,74 @@
+﻿using System;
+
+class Program
+{
+    public static void FillMatrix(int[,] matrix)
+    {
+        Random random = new Random();
+        for (int i = 0; i < matrix.GetLength(0); i++)
+        {
+            for (int j = 0; j < matrix.GetLength(1); j++)
+            {
+                matrix[i, j] = random.Next(0, 17);
+            }
+        }
+
+        for (int i = 0; i < matrix.GetLength(0); i++)
+        {
+            for (int j = 0; j < matrix.GetLength(1); j++)
+            {
+                Console.Write(matrix[i, j] + " ");
+            }
+            Console.WriteLine();
+        }
+    }
+
+    public static void CalculateColorData(int[,] matrix)
+    {
+        int maxLength = 0;
+        int startRow = 0, startCol = 0, endRow = 0, endCol = 0;
+
+        for (int i = 0; i < matrix.GetLength(0); i++)
+        {
+            int currentLength = 1;
+            for (int j = 0; j < matrix.GetLength(1) - 1; j++)
+            {
+                if (matrix[i, j] == matrix[i, j + 1])
+                {
+                    currentLength++;
+                }
+                else
+                {
+                    if (currentLength > maxLength)
+                    {
+                        maxLength = currentLength;
+                        startRow = i;
+                        startCol = j - currentLength + 1;
+                        endRow = i;
+                        endCol = j;
+                    }
+                    currentLength = 1;
+                }
+            }
+            if (currentLength > maxLength)
+            {
+                maxLength = currentLength;
+                startRow = i;
+                startCol = matrix.GetLength(1) - currentLength;
+                endRow = i;
+                endCol = matrix.GetLength(1) - 1;
+            }
+        }
+
+        Console.WriteLine($"The longest line has length {maxLength} and color is {matrix[startRow, startCol]} colors");
+        Console.WriteLine($"Start of line: [{startRow}, {startCol}]");
+        Console.WriteLine($"End of line: [{endRow}, {endCol}]");
+    }
+    static void Main()
+    {
+        int[,] matrix = new int[8, 8];
+
+        FillMatrix(matrix);
+        CalculateColorData(matrix);
+    }
+}
