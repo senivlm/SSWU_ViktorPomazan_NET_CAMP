11a2bba6d8899f63eb78e59688ebca13db111d99
Kirieshka <67062457+Kirieshke@users.noreply.github.com>
2023-04-03 23:25:18 +0300

    init

---
+++ Home_task_1/exercise_4/exercise_4/Program.cs
@@ -0,0 +1,58 @@
+﻿using System.Text;
+
+namespace Exercise4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = 3;
+            Tensor<int> tensor3d = new Tensor<int>(n, n, n);
+            Tensor<int> tensor2d = new Tensor<int>(n, n);
+            Tensor<int> tensor = new Tensor<int>(n);
+            Tensor<int> tensor_number = new Tensor<int>(1);
+
+            Random rand = new Random();
+            Console.WriteLine("Display 3х3х3 tensor");
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    for (int k = 0; k < n; k++)
+                    {
+                        tensor3d.SetValue(rand.Next(0, 10), i, j, k);
+                    }
+                }
+            }
+
+            tensor3d.Print3DTensor(tensor3d, n);
+            
+           
+            Console.WriteLine("Display 3х3 tensor");
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    tensor2d.SetValue(rand.Next(0, 10), i, j);
+                }
+            }
+            tensor2d.Print2DTensor(tensor2d, n);
+            
+
+            Console.WriteLine("Display vector");
+            
+            for (int i = 0; i < n; i++)
+            {
+                tensor.SetValue(rand.Next(0, 10), i);
+
+            }
+
+            tensor.PrintTensor(tensor, n);
+            Console.WriteLine();
+            Console.WriteLine("Display number");
+            
+            tensor_number.SetValue(rand.Next(1,10), 0);
+            tensor_number.PrintNumber(tensor_number);
+        }
+    }
+}
---
+++ Home_task_1/exercise_4/exercise_4/Tensor.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise4
+{
+    internal class Tensor<T>
+    {
+        private readonly int[] _shape;
+        private readonly T[] _data;
+
+        public Tensor(params int[] shape)
+        {
+            if (shape == null || shape.Length == 0)
+            {
+                throw new ArgumentException("At least one shape is required.", nameof(shape));
+            }
+
+            _shape = shape;
+            _data = new T[CalculateTensorSize(shape)];
+        }
+
+        public int[] Dimensions => _shape;
+
+        public T GetValue(params int[] indices)
+        {
+            if (indices == null || indices.Length != _shape.Length)
+            {
+                throw new ArgumentException("Indices must have the same length as the tensor's shape.", nameof(indices));
+            }
+
+            int offset = CalculateIndex(indices);
+            return _data[offset];
+        }
+        public void Print3DTensor(Tensor<int> tensor3d, int n)
+        {
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    for (int k = 0; k < n; k++)
+                    {
+                        Console.Write(tensor3d.GetValue(i, j, k) + " ");
+                    }
+
+                    Console.WriteLine();
+                }
+                Console.WriteLine();
+            }
+        }
+        public void Print2DTensor(Tensor<int> tensor2d, int n)
+        {
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    Console.Write(tensor2d.GetValue(i, j) + " ");
+                }
+                Console.WriteLine();
+            }
+        }
+        public void PrintTensor(Tensor<int> vector, int n)
+        {
+            for (int i = 0; i < n; i++)
+            { 
+                Console.Write(vector.GetValue(i) + " ");
+            }
+        }
+        public void PrintNumber(Tensor<int> number)
+        {
+            Console.WriteLine(number.GetValue(0));
+        }
+        public void SetValue(T data, params int[] indices)
+        {
+            if (indices == null || indices.Length != _shape.Length)
+            {
+                throw new ArgumentException("Indices must have the same length as the tensor's shape.", nameof(indices));
+            }
+
+            int offset = CalculateIndex(indices);
+            _data[offset] = data;
+        }
+
+        private int CalculateTensorSize(int[] shape)
+        {
+            int size = 1;
+            foreach (int sh in shape)
+            {
+                if (sh <= 0)
+                {
+                    throw new ArgumentException("Shape must be positive.", nameof(shape));
+                }
+
+                size *= sh;
+            }
+
+            return size;
+        }
+
+        private int CalculateIndex(int[] indices)
+        {
+            int offset = 0;
+            int stride = 1;
+
+            for (int i = _shape.Length - 1; i >= 0; i--)
+            {
+                int index = indices[i];
+                if (index < 0 || index >= _shape[i])
+                {
+                    throw new IndexOutOfRangeException("Index out of range");
+                }
+
+                offset += index * stride;
+                stride *= _shape[i];
+            }
+
+            return offset;
+        }
+
+    }
+}
