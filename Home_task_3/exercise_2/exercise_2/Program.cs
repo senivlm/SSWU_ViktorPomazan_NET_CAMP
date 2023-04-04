a7b6b4e2b454ac34af1ce48f9dee3067986a0e31
Kirieshka <67062457+Kirieshke@users.noreply.github.com>
2023-04-04 22:22:55 +0300

    create

---
+++ Home_task_3/exercise_2/exercise_2/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace exercise_2
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.Write("Enter the text: ");
+            string text = Console.ReadLine();
+
+            Console.Write("Enter the substring: ");
+            string substring = Console.ReadLine();
+
+            Console.Write("Enter the replacement: ");
+            string replacement = Console.ReadLine();
+
+            int? secondSubstringIndex = StringManipulator.FindSecondSubstringIndex(text, substring);
+            if (secondSubstringIndex != null)
+            {
+                Console.WriteLine("Second '" + substring + "' found at index " + secondSubstringIndex);
+            }
+            else
+            {
+                Console.WriteLine("Second '" + substring + "' not found");
+            }
+
+            int count = StringManipulator.FIndCountWordsStartingWithCapitalLetter(text);
+            Console.WriteLine("Number of words starting with a capital letter: " + count);
+
+            string replacedText = StringManipulator.ReplaceWordsWithDoubleLetters(text, replacement);
+            Console.WriteLine("Text with words containing double letters replaced: " + replacedText);
+        }
+    }
+}
---
+++ Home_task_3/exercise_2/exercise_2/StringManipulator.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace exercise_2
+{
+    internal class StringManipulator
+    {
+        public static int? FindSecondSubstringIndex(string text, string substring)
+        {
+            int firstIndex = text.IndexOf(substring);
+            if (firstIndex != -1)
+            {
+                int secondIndex = text.IndexOf(substring, firstIndex + 1);
+                if (secondIndex != -1)
+                {
+                    return secondIndex;
+                }
+            }
+            return null;
+        }
+
+        public static int FIndCountWordsStartingWithCapitalLetter(string text)
+        {
+            int count = 0;
+            string[] words = text.Split(new char[] { ' ', '\t', '\n', '\r' }, StringSplitOptions.RemoveEmptyEntries);
+            foreach (string word in words)
+            {
+                if (Char.IsUpper(word[0]))
+                {
+                    count++;
+                }
+            }
+            return count;
+        }
+
+        public static string ReplaceWordsWithDoubleLetters(string text, string replacement)
+        {
+            Regex regex = new Regex(@"\b\w*(\w)\1\w*\b");
+            return regex.Replace(text, replacement);
+        }
+    }
+
+}
