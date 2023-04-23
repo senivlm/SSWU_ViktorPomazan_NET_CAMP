d338f3566eda32eddbe7050e6d30a3e87a473fb9
senivlm <91743906+senivlm@users.noreply.github.com>
2023-04-23 18:20:58 +0300

    Update Program.cs

--- Home_task_5/exercise_2/exercise_2/Program.cs
+++ Home_task_5/exercise_2/exercise_2/Program.cs
@@ -1,7 +1,7 @@
 ﻿using exercise_2;
 
 namespace exercise_2
-{
+{// У Вас, на жаль, не враховано можливості додавати довільну кількість рівнів, а також знову не побачила копії екрана.
     class Program
     {
         static void Main(string[] args)
